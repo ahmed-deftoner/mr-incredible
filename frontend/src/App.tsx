12cf9a5356bf895f696f6d885d0099c366aa1ed7
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-03 20:01:00 +0500

    some styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-       <button>UNCANNY</button>
-       <button>ANGRY</button>
-       <button>CANNY</button>
-    </div>
+      <div className="flex justify-center mt-56 flex-col gap-3 md:flex-row">
+        <button>UNCANNY</button>
+        <button>ANGRY</button>
+        <button>CANNY</button>
+      </div>
   )
 }
 
