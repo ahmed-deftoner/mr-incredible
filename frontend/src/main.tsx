662a11f9fc93e8b8ed328ebeb27162073788d7d6
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-03 20:11:01 +0500

    routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import App from './App'
 import './index.css'
+import Angry from './routes/angry'
+import Uncanny from './routes/uncanny'
+import Canny from './routes/canny'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="angry" element={<Angry />} />
+        <Route path="canny" element={<Canny />} />
+        <Route path="uncanny" element={<Uncanny />} />
+      </Route>
+    </Routes>
   </BrowserRouter>
 )
