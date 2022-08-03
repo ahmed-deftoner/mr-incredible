aacc6272ad00611395efd3a3caea3b6896c8a959
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-03 20:21:43 +0500

    style link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,9 @@ function App() {
 
   return (
       <div className="flex justify-center mt-56 flex-col gap-3 md:flex-row">
-          <Link to="/uncanny">UNCANNY</Link>
-          <Link to="/angry">ANGRY</Link>
-          <Link to="/canny">CANNY</Link>
+          <Link className="bg-zinc-800 px-8 py-5 text-white" to="/uncanny">UNCANNY</Link>
+          <Link className="bg-zinc-800 px-8 py-5 text-white" to="/angry">ANGRY</Link>
+          <Link className="bg-zinc-800 px-8 py-5 text-white" to="/canny">CANNY</Link>
           <Outlet/>
       </div>
   )
