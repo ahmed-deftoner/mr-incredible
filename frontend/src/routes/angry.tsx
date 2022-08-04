0c11c0db2bb2d3accf7067f1a6ba72e750658018
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-04 17:16:15 +0500

    useffect

--- frontend/src/routes/angry.tsx
+++ frontend/src/routes/angry.tsx
@@ -1,8 +1,27 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
 
 const angry = memo(() => {
+
+  const imgs:Array<string> = [];
+
+  useEffect(() => {
+    for (let index = 0; index < 10; index++) {
+      imgs[index] = 'i' + (index+1)
+      console.log(imgs[index])
+    }
+  },)
+  
   return (
-    <div>angry</div>
+    <div>
+      <div className='flex flex-row justify-center'>
+          {imgs.map((item,index) => (
+            <>
+              <img src=''></img>
+              <input>hsjs</input>
+            </>
+          ))}
+      </div>
+    </div>
   )
 })
 
