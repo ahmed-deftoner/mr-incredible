81ccf6d028740d90bc2f438e37239b8537d585d1
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-04 17:35:31 +0500

    ui change

--- frontend/src/routes/angry.tsx
+++ frontend/src/routes/angry.tsx
@@ -15,13 +15,16 @@ const angry = memo(() => {
   
   return (
     <div>
-      <div className='flex flex-col justify-center'>
+      <div className='flex flex-col justify-center items-center mt-20'>
           {imgs.map((item,index) => (
-            <li key={index} className="flex flex-row">
-              <img src=''></img>
+            <li key={index} className="flex flex-row gap-5 mb-5">
+              <img className='h-50 w-48' src={'../../angry/'+item+'.png'}></img>
               <input></input>
             </li>
           ))}
+          <button>
+            Generate meme
+          </button>
       </div>
     </div>
   )
